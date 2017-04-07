1217e0ec56a3623275cbde2cb43122d24bfcd0ba
Alistair Brown <github@alistairjcbrown.com>
2017-04-07 01:05:02 +0100

    Expose module interface

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # leagueofcomicgeeks
 Unofficial node.js library for interacting with League of Comic Geeks
+
+## To do
+
+ - Tests
+   - [ ] Unit tests
+   - [ ] Integration tests
+ - Tooling
+   - [ ] Linting
+   - [ ] Documentation
+   - [ ] Publish module
+
+ - Can we dynamically load the publishers list in / update it periodically?
+   - [ ] Perhaps a publisher module which can be called to get latest, which by default falls back to JSON file
--- index.js
+++ index.js
@@ -1,54 +1,10 @@
-var _ = require('lodash');
-var login = require('./src/login');
-var wishList = require('./src/wish-list');
-var readList = require('./src/read-list');
-var collection = require('./src/collection');
-var pullList = require('./src/pull-list');
-var newComics = require('./src/new-comics');
-var searchResults = require('./src/search-results');
-var types = require('./src/utils/types');
-
-/*
- * X Wish list
- * X Read list
- * X Collection
- * X Pull list
- * X Load auth details in
- * X New Comics
- * X Searching
- * X All lists above are series based, can we get comic based too?
- * X Standardise dates - how they're accepted, internal usage, data normalisation
- * X Bulk actions on series level
- * X Optional options - default type to 'issue'
- * X Filters for collection, new comics, etc.
- *
- * - Expose interface from this file
- *
- * Tests
- *  - Unit tests
- *  - Integration tests
- * Tooling
- *  - Linting
- *  - Documentation
- *  - Publish module
- *
- * - Can we dynamically load the publishers list in / update it periodically?
- */
-
-login('lofcg_test', '<password>', function (err, userId) {
-  pullList.get(userId, '2017-04-12', { publishers: ['Dark Horse Comics', 'Marvel Comics'] }, function () {
-    console.log("Added to pull list", arguments);
-  });
-  // wishList.get(userId, {}, function (err, wishlist) {
-  //   console.log("Wish list", arguments);
-  //   pullList.get(userId, '2017-03-22', { type: types.ISSUE }, function (err, pullList) {
-  //     console.log("pull list", arguments);
-      // wishList.add(pullList[0].id, function () {
-      //   console.log("Wishlist add", arguments);
-      //   wishList.get(userId, { type: types.ISSUE }, function (err, wishlist) {
-      //     console.log("Wish list", arguments);
-      //   });
-      // });
-  //   });
-  // });
-});
+module.exports = {
+  types: require('./src/utils/types'),
+  login: require('./src/login'),
+  searchResults: require('./src/search-results'),
+  newComics: require('./src/new-comics'),
+  collection: require('./src/collection'),
+  pullList: require('./src/pull-list'),
+  wishList: require('./src/wish-list'),
+  readList: require('./src/read-list'),
+};
