71472886a5d4ad0d1820a2f7ddf1d34eb2ea4797
Alistair Brown <github@alistairjcbrown.com>
2018-06-24 23:01:41 +0100

    Enhance data pulled

--- src/utils/extract-data-from.js
+++ src/utils/extract-data-from.js
@@ -17,19 +17,27 @@ const sortList = function (list, sortBy = 'asc') {
   return list;
 };
 
+const buildSeriesUrl = function (id, coverUrl) {
+  const slug = coverUrl.match(/\/\d+-(.+)\./)[1];
+  return `/comics/series/${id}/${slug}`;
+};
+
 const seriesExtractor = function (response, options) {
   const $ = cheerio.load(response.list);
 
   const extractSeriesData = function () {
-    const $name = $(this).find('.name a');
-    const cover = $(this).find('.cover img').attr('data-original');
-    const publisher = $(this).find('.publisher').text().trim();
-    const count = parseInt($(this).find('.details').text().trim(), 10);
-    const series = $(this).find('.series').text().trim();
+    const $el = $(this);
+    const id = $el.find('.name a').attr('data-id');
+    const name = $el.find('.name a').text().trim();
+    const cover = $el.find('.cover img').attr('data-original').split('?')[0];
+    const publisher = $el.find('.publisher').text().trim();
+    const count = parseInt($el.find('.details').text().trim(), 10);
+    const series = $el.find('.series').text().trim();
 
     return {
-      id: $name.attr('data-id'),
-      name: $name.text().trim(),
+      id,
+      name,
+      url: config.rootUrl + buildSeriesUrl(id, cover),
       cover: config.rootUrl + cover,
       publisher,
       count,
@@ -40,31 +48,84 @@ const seriesExtractor = function (response, options) {
   return sortList($('li').map(extractSeriesData).get(), options.sort);
 };
 
+const getVote = (piece, boundary) => parseFloat(piece.match(new RegExp(`${boundary}([^%]+)%`))[1].trim());
+const getVotes = function ($el) {
+  const consensus = 'consensus:';
+  const potw = 'potw:';
+
+  const toolbarPieces = $el.find('.comic-list-toolbar-text')
+    .text()
+    .trim()
+    .replace(/·/g, '')
+    .split('    ')
+    .map(value => value.trim().toLowerCase())
+    .filter(value => value !== '');
+
+  return toolbarPieces.reduce(function (votes, piece) {
+    if (piece.includes(consensus)) return _.extend({}, votes, { consensusVote: getVote(piece, consensus) });
+    if (piece.includes(potw)) return _.extend({}, votes, { pickOfTheWeekVote: getVote(piece, potw) });
+    return votes;
+  }, { consensusVote: null, pickOfTheWeekVote: null });
+};
+
+const getCollectionStats = function ($, $el) {
+  const collectionStats = $el.find('.comic-list-stats a')
+    .map(function () {
+      const $statButton = $(this);
+      const actionText = $statButton.find('.text').text().toLowerCase();
+      const counterValue = parseInt($statButton.find('.counter').text(), 10);
+
+      if (actionText.startsWith('add')) return { added: counterValue };
+      if (actionText.startsWith('pull')) return { pulled: counterValue };
+      return {};
+    })
+    .get();
+
+  return _.extend({
+    pulled: null,
+    added: null
+  }, ...collectionStats);
+};
+
 const issueExtractor = function (response, options) {
   const $ = cheerio.load(response.list);
 
   const extractIssueData = function () {
-    const id = $(this).attr('id').replace('comic-', '').trim();
-    const name = $(this).find('.comic-title').text().trim();
-    const cover = $(this).find('.comic-cover-art img').attr('data-original');
+    const $el = $(this);
+    const url = $el.find('.comic-title a').attr('href').trim();
+    const [, id, variantId = null] = url.match(/comic\/(\d+)\/.+?(?:\?variant=(\d+))?$/);
+    const name = $el.find('.comic-title a').text().trim();
+    const cover = $el.find('.comic-cover-art img').attr('data-original').split('?')[0];
+    const diamondSku = $el.find('.comic-diamond-sku').text().trim() || null;
+    const { consensusVote, pickOfTheWeekVote } = getVotes($el);
+    const { pulled, added } = getCollectionStats($, $el);
 
-    const comicDetails = $(this).find('.comic-details').text().split('·');
+    const comicDetails = $el.find('.comic-details').text().split('·');
     const publisher = (comicDetails[0] || '').trim();
     const releaseDate = convertToISO8601Date((comicDetails[1] || '').trim());
     const price = (comicDetails[2] || '').trim();
 
-    const $description = $(this).find('.comic-description p');
+    const $description = $el.find('.comic-description p');
     $description.find('a').remove();
     const description = $description.text().trim();
 
     return {
       id,
+      variantId,
+      url: config.rootUrl + url,
       name,
-      cover: config.rootUrl + cover,
+      cover: cover.includes('no-cover') ? null : config.rootUrl + cover,
       publisher,
       description,
       releaseDate,
-      price
+      price,
+      diamondSku,
+      userMetrics: {
+        pulled,
+        added,
+        consensusVote,
+        pickOfTheWeekVote
+      }
     };
   };
 
--- test/utils/custom-matchers.js
+++ test/utils/custom-matchers.js
@@ -3,20 +3,32 @@ const fs = require('fs-extra');
 const _ = require('lodash');
 const moment = require('moment');
 
-const expectedIssueKeys = ['id', 'name', 'cover', 'publisher', 'description', 'releaseDate', 'price'];
-const expectedSeriesKeys = ['id', 'name', 'cover', 'publisher', 'count', 'series'];
+const expectedIssueKeys = [
+  'id',
+  'variantId',
+  'name',
+  'url',
+  'cover',
+  'publisher',
+  'description',
+  'releaseDate',
+  'price',
+  'diamondSku',
+  'userMetrics'
+];
+const expectedSeriesKeys = ['id', 'name', 'url', 'cover', 'publisher', 'count', 'series'];
 const expectedSessionKeys = ['id', 'username', 'email', 'session'];
 
-const createFailure = function (message) {
-  return { pass: false, message };
+const createFailure = function (message, receivedValue) {
+  return { pass: false, message: `${message} - Received ${JSON.stringify(receivedValue)}` };
 };
 
 const isNonEmptyString = function (value) {
   return _.isString(value) && !_.isEmpty(value);
 };
 
-const isPositiveNumber = function (value) {
-  return _.isNumber(value) && value > 0;
+const isNonNegativeNumber = function (value) {
+  return _.isNumber(value) && value >= 0;
 };
 
 const isUrl = function (value) {
@@ -42,13 +54,55 @@ module.exports = {
           return createFailure('Unexpected keys in comic issue object');
         }
 
-        if (!isNonEmptyString(actual.id)) return createFailure('Invalid id in comic issue object');
-        if (!isNonEmptyString(actual.name)) return createFailure('Invalid name in comic issue object');
-        if (!isUrl(actual.cover)) return createFailure('Invalid cover URL in comic issue object');
-        if (!isNonEmptyString(actual.publisher)) return createFailure('Invalid publisher in comic issue object');
-        if (_.isUndefined(actual.description)) return createFailure('Invalid description in comic issue object');
-        if (!isDate(actual.releaseDate)) return createFailure('Invalid release date in comic issue object');
-        if (_.isUndefined(actual.price)) return createFailure('Invalid price in comic issue object');
+        const {
+          id, variantId, url, name, cover, publisher, description, releaseDate, price, diamondSku, userMetrics
+        } = actual;
+
+        if (!isNonEmptyString(id)) {
+          return createFailure('Invalid id in comic issue object', id);
+        }
+        if (!isNonEmptyString(variantId) && !_.isNull(variantId)) {
+          return createFailure('Invalid variant id in comic issue object', variantId);
+        }
+        if (!isUrl(url)) {
+          return createFailure('Invalid URL in comic issue object', url);
+        }
+        if (!isNonEmptyString(name)) {
+          return createFailure('Invalid name in comic issue object', name);
+        }
+        if (!isUrl(cover) && !_.isNull(cover)) {
+          return createFailure('Invalid cover URL in comic issue object', cover);
+        }
+        if (!isNonEmptyString(publisher)) {
+          return createFailure('Invalid publisher in comic issue object', publisher);
+        }
+        if (_.isUndefined(description)) {
+          return createFailure('Invalid description in comic issue object', description);
+        }
+        if (!isDate(releaseDate)) {
+          return createFailure('Invalid release date in comic issue object', releaseDate);
+        }
+        if (_.isUndefined(price)) {
+          return createFailure('Invalid price in comic issue object', price);
+        }
+        if (!isNonEmptyString(diamondSku) && !_.isNull(diamondSku)) {
+          return createFailure('Invalid Diamond SKU in comic issue object', diamondSku);
+        }
+        if (!isNonNegativeNumber(userMetrics.pulled) && !_.isNull(userMetrics.pulled)) {
+          return createFailure('Invalid pulled user metric in comic issue object', userMetrics.pulled);
+        }
+        if (!isNonNegativeNumber(userMetrics.added) && !_.isNull(userMetrics.added)) {
+          return createFailure('Invalid added user metric in comic issue object', userMetrics.added);
+        }
+        if (!isNonNegativeNumber(userMetrics.consensusVote) && !_.isNull(userMetrics.consensusVote)) {
+          return createFailure('Invalid consensus vote user metric in comic issue object', userMetrics.consensusVote);
+        }
+        if (!isNonNegativeNumber(userMetrics.pickOfTheWeekVote) && !_.isNull(userMetrics.pickOfTheWeekVote)) {
+          return createFailure(
+            'Invalid pick of the week vote user metric in comic issue object',
+            userMetrics.pickOfTheWeekVote
+          );
+        }
 
         return { pass: true };
       }
@@ -62,12 +116,16 @@ module.exports = {
           return createFailure('Unexpected keys in comic series object');
         }
 
-        if (!isNonEmptyString(actual.id)) return createFailure('Invalid id in comic series object');
-        if (!isNonEmptyString(actual.name)) return createFailure('Invalid name in comic series object');
-        if (!isUrl(actual.cover)) return createFailure('Invalid cover URL in comic series object');
-        if (!isNonEmptyString(actual.publisher)) return createFailure('Invalid publisher in comic series object');
-        if (!isPositiveNumber(actual.count)) return createFailure('Invalid count in comic series object');
-        if (_.isUndefined(actual.series)) return createFailure('Invalid series in comic series object');
+        const {
+          id, url, name, cover, publisher, count, series
+        } = actual;
+        if (!isNonEmptyString(id)) return createFailure('Invalid id in comic series object', id);
+        if (!isUrl(url)) return createFailure('Invalid URL in comic series object', url);
+        if (!isNonEmptyString(name)) return createFailure('Invalid name in comic series object', name);
+        if (!isUrl(cover)) return createFailure('Invalid cover URL in comic series object', cover);
+        if (!isNonEmptyString(publisher)) return createFailure('Invalid publisher in comic series object', publisher);
+        if (!isNonNegativeNumber(count)) return createFailure('Invalid count in comic series object', count);
+        if (_.isUndefined(series)) return createFailure('Invalid series in comic series object', series);
 
         return { pass: true };
       }
@@ -81,11 +139,14 @@ module.exports = {
           return createFailure('Unexpected keys in session object');
         }
 
-        if (actual.id !== expected.id) return createFailure('Unexpected id in session object');
-        if (actual.username !== expected.username) return createFailure('Unexpected username in session object');
-        if (!isNonEmptyString(actual.email)) return createFailure('Invalid email in session object');
-        if (!isNonEmptyString(actual.session) || actual.session.length < 500) {
-          return createFailure('Invalid session in session object');
+        const {
+          id, username, email, session
+        } = actual;
+        if (id !== expected.id) return createFailure('Unexpected id in session object', id);
+        if (username !== expected.username) return createFailure('Unexpected username in session object', username);
+        if (!isNonEmptyString(email)) return createFailure('Invalid email in session object', email);
+        if (!isNonEmptyString(session) || session.length < 500) {
+          return createFailure('Invalid session in session object', session);
         }
 
         return { pass: true };
@@ -105,7 +166,7 @@ module.exports = {
         } catch (e) {
           fs.outputJsonSync(snapshotPath, actual, { spaces: 4 });
           // eslint-disable-next-line no-console
-          console.warn(`Snapshot '${snapshotName}' created. Please re-run the tests.`);
+          console.warn(`\nSnapshot '${snapshotName}' created. Please re-run the tests.`);
           return { pass: false, message: () => 'Snapshot missing' };
         }
 
